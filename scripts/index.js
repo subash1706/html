ee98f687c9f9ca8ce654f8bf31003abbf43299e9
subash1706 <subash.somasundaram@chainsys.com>
2022-03-15 22:00:56 +0530

    15march
    Signed-off-by: subash1706 <subash.somasundaram@chainsys.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,22 @@
-const share = document.querySelector(".share");
-const overlay = document.querySelector(".info-and-share");
-
-share.addEventListener("click", () => {
-  if (overlay.classList.contains("hidden")) {
-    overlay.classList.add("active");
-    overlay.classList.remove("hidden");
-  } else {
-    overlay.classList.add("hidden");
-    overlay.classList.remove("active");
-  }
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
 });
